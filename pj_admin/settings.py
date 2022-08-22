ff8132c16a47578ef2076e0d2106d6339cf263a9
unohayato <mamidori1p1p@gmail.com>
2022-08-22 21:17:42 +0900

    settings

--- pj_admin/settings.py
+++ pj_admin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$7$^%8fn6vs4tu$39u33p+^(5mjn)_p1jwtwzrfl(rir2m$z1_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
